912296eee033342fa21fe9c96da989e5f4331b77
Akhil kumar <154546670+AkhilDevOps108@users.noreply.github.com>
2025-07-12 17:28:23 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,5 @@
 provider "aws" {
-  region = var.aws_region
+  region     = var.aws_region
+  access_key = var.aws_access_key       # Optional if using env vars
+  secret_key = var.aws_secret_key       # Optional if using env vars
 }
