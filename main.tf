e0c9695c6d00410eb6ef3a884420b89965499002
Akhil kumar <154546670+AkhilDevOps108@users.noreply.github.com>
2025-07-12 17:46:38 +0530

    wee

--- main.tf
+++ main.tf
@@ -22,3 +22,8 @@ resource "aws_s3_bucket_public_access_block" "example" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
+resource "aws_s3_bucket" "app_bucket" {
+  bucket = "${var.project_name}-bucket"
+
+  force_destroy = true  # <--- Add this line
+}
