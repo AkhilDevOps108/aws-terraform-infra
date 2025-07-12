57e12a710e627ff63801a5090301d5380fdaee3b
Akhil kumar <154546670+AkhilDevOps108@users.noreply.github.com>
2025-07-12 17:49:52 +0530

    wertg

--- main.tf
+++ main.tf
@@ -1,12 +1,13 @@
 resource "aws_s3_bucket" "app_bucket" {
-  bucket = "${var.project_name}-bucket"
+  bucket         = "${var.project_name}-bucket"
+  force_destroy  = true
 
   tags = {
     Name = "${var.project_name}-bucket"
   }
 }
 
-resource "aws_s3_bucket_ownership_controls" "example" {
+resource "aws_s3_bucket_ownership_controls" "ownership" {
   bucket = aws_s3_bucket.app_bucket.id
 
   rule {
@@ -14,16 +15,7 @@ resource "aws_s3_bucket_ownership_controls" "example" {
   }
 }
 
-resource "aws_s3_bucket_public_access_block" "example" {
+resource "aws_s3_bucket_public_access_block" "public_access" {
   bucket = aws_s3_bucket.app_bucket.id
 
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
-resource "aws_s3_bucket" "app_bucket" {
-  bucket = "${var.project_name}-bucket"
-
-  force_destroy = true  # <--- Add this line
-}
+  b
