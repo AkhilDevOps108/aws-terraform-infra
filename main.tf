1971318382c15e6379c5b042a8e42def61955952
Akhil kumar <154546670+AkhilDevOps108@users.noreply.github.com>
2025-07-12 17:23:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,8 @@
 resource "aws_s3_bucket" "app_bucket" {
   bucket = "${var.project_name}-bucket"
-  acl    = "private"
 }
 
+resource "aws_s3_bucket_acl" "bucket_acl" {
+  bucket = aws_s3_bucket.app_bucket.id
+  acl    = "private"
+}
